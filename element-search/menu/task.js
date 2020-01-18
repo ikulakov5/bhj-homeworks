7bb217def077445a6727c868a91e98df76c9a802
iKulakov5 <codykulakov@gmail.com>
2020-01-18 21:43:11 +0300

    hw changed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -7,10 +7,20 @@ for(let i = 0; i < subMenu.length; i++){
 	menuExistSub[i] = parentList.querySelector("a.menu__link");
 
 	menuExistSub[i].onclick = function() {
-		for(let z = 0; z < subMenu.length; z++){
+
+		if(subMenu[i].className == "menu menu_sub menu_active"){
+			subMenu[i].className = "menu menu_sub";
+			console.log('1')
+			return false
+		} else {
+
+		for(let z = 0; z < subMenu.length; z++) {
 			subMenu[z].className = "menu menu_sub";
 		}
 		subMenu[i].className = "menu menu_sub menu_active";
-		return false;
+		return false
+
 	}
+
+}
 }
