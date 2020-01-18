c2fea52880f7f6960527e39154a8adda904ec238
iKulakov5 <codykulakov@gmail.com>
2020-01-18 17:00:36 +0300

    changes done

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,7 +1,7 @@
 let modalMain = document.getElementById("modal_main");
 modalMain.classList.add("modal_active");
 
-let closeBtn = document.getElementsByClassName("modal__close modal__close_times");
+let closeBtn = document.getElementsByClassName("modal__close_times");
 let close = closeBtn.item(0);
 let closeSec = closeBtn.item(1);
 close.onclick = function() {
@@ -10,6 +10,9 @@ close.onclick = function() {
 
 let showSuccess = document.getElementsByClassName("show-success");
 showSuccess[0].onclick = function() {
+	modalMain.onclick = function() {
+		modalMain.classList.remove("modal_active");
+	}
 	let modalSuccess = document.getElementById("modal_success");
 	modalSuccess.classList.add("modal_active");
 	closeSec.onclick = function() {
