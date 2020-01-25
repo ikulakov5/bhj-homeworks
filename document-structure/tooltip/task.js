2db07da8163ec84f10055f986100ac6f101247a0
iKulakov5 <codykulakov@gmail.com>
2020-01-25 18:24:18 +0300

    done

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,26 @@
+let tips = document.querySelectorAll('.has-tooltip');
+
+for (let tip of tips) {
+	tip.addEventListener('click', (e) => {
+		let tipTitle = e.target.getAttribute('title');
+		let activeTooltip = document.querySelector(".tooltip_active");
+
+		if(activeTooltip) {
+			if (activeTooltip.textContent != tipTitle) {
+                activeTooltip.remove();
+				let left = tip.getBoundingClientRect().left;
+				let top = tip.getBoundingClientRect().top;
+				tip.insertAdjacentHTML("afterEnd", `<div class="tooltip tooltip_active" style="left: ${left}px; top: ${top}px">${tipTitle}</div>`);
+		        } else {
+                activeTooltip.remove();
+        		}
+ 		} else {
+		let left = tip.getBoundingClientRect().left;
+		let top = tip.getBoundingClientRect().top;
+
+		tip.insertAdjacentHTML("afterEnd", `<div class="tooltip tooltip_active" style="left: ${left}px; top: ${top}px">${tipTitle}</div>`);
+		}
+
+		e.preventDefault();
+	});
+}
