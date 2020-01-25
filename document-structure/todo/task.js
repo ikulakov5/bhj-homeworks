cda31e95e9fff301c6c034714d3e3543b57c0de5
iKulakov5 <codykulakov@gmail.com>
2020-01-25 18:52:49 +0300

    done

---
+++ document-structure/todo/task.js
@@ -0,0 +1,23 @@
+let taskInput = document.getElementById("task__input");
+let tasks = document.getElementById("tasks__list");
+let addBtn = document.getElementById("tasks__add");
+
+
+addBtn.addEventListener("click", (e) => {
+	if(taskInput.value != '') {
+
+		tasks.innerHTML += `<div class="task"><div class="task__title">${taskInput.value}</div><a href="#" class="task__remove">&times;</a></div>`;
+		taskInput.value = '';
+
+		e.preventDefault();
+	}
+
+});
+
+window.onclick = (e) => {
+    if (e.target.classList.contains('task__remove')) {
+        e.target.parentElement.remove();
+    }
+}
+
+
